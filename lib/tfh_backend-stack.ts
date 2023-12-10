8268bbbf6dadc71eed7b5b2ed91a6be872824583
Keshav Gupta <keshav.pg@gmail.com>
2023-12-10 23:54:53 +0100

    Dummy s3 bucket cdk deployment test

--- lib/tfh_backend-stack.ts
+++ lib/tfh_backend-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,8 @@ export class TfhBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TfhBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
   }
 }
