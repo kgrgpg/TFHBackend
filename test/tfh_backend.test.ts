1c11e27bb3b507d9f693f9c69023e2154550f9a1
Keshav Gupta <keshav.pg@gmail.com>
2023-12-10 23:21:01 +0100

    AWS CDK initialization

---
+++ test/tfh_backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TfhBackend from '../lib/tfh_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tfh_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TfhBackend.TfhBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
